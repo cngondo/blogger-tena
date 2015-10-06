50ed58a06c63df2354e67d5385448fe9fafbe2d4
cngondo <devngondo@gmail.com>
2015-10-06 19:12:10 +0300

    Passing params in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    # We don't need a template for some actions. just redirects
+    # Make sure you have the params included
+    @article = Article.new(article_params)
+    @article.save
+    # This the page to redirect to
+    redirect_to article_path(@article)
+  end
+
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,6 @@
 module ArticlesHelper
+    #The class that passes the strong params to the create
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
