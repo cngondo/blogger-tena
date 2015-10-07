b084231c85cc9a16ca4fc5b84a9a13453531ebbe
cngondo <devngondo@gmail.com>
2015-10-08 00:29:37 +0300

    Models for working with tags

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
     #The class that passes the strong params to the create
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  # Initially it ised to be like
+  # tags = article.taggings.collect{|tagging| tagging.tag}
+  # Which is PAIN!! BUt we have the 'through' keyword that assists us
+  has_many :tags, through: :taggings
+  # This lists the tags
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+  def tag_list=(tags_string)
+  end
 end
